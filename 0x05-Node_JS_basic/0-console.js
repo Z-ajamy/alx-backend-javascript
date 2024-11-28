2a5e85bff4e6c7389e7bb84d9e6ceef6f834e1ff
Z-ajamy <abdorahman0283@gmail.com>
2024-11-28 12:34:36 +0200

    0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a string argument to the standard output.
+ * @param {string} message - The message to print.
+ */
+function displayMessage(message) {
+    console.log(message);
+  }
+  
+  module.exports = displayMessage;
+  
